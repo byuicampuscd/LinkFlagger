913d60c4b7a9297fb60e8f6464fa582828957f37
didymusbenson <didymus.benson@gmail.com>
2017-01-04 09:59:32 -0700

    fixes #5

--- egg.js
+++ egg.js
@@ -5,7 +5,7 @@ if (document.title == "Login - Brigham Young University - Idaho") {
         window.addEventListener("keydown", function (e) {
             kkeys.push(e.keyCode);
             if (kkeys.toString().indexOf(konami) >= 0) {
-                alert("Didymus says hello!");
+                $('body').append("<style>.cat{position:fixed; right:100%; bottom:-275px;  animation: party 8s forwards;}  @keyframes party { 100% {right:10%;}}</style><img class='cat' src='https://i.imgur.com/Cii6SuE.gif'>");
                 kkeys = [];
             }
         }, true);
