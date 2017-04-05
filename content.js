5862069747afc41a8df4a4f56613b43d587769d0
Ben Harker <har05054@byui.edu>
2017-04-05 09:09:30 -0600

    fixed issue where settings were defaulting to display on when not on for first launch

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+/*eslint-env browser*/
+/*global chrome*/
 // ==LINK FLAGGER==================================================
 // @name        LinkFlagger 2.0
 // @version     0.0
@@ -9,47 +11,45 @@
 // SETTINGS CHECKERS
 var htmlON;
 chrome.storage.sync.get("htmlsetting", function (result) {
-    htmlON = result.htmlsetting;
-    console.log(result.htmlsetting);
-     tryhtml(htmlON);
+  htmlON = result.htmlsetting;
+  tryhtml(htmlON);
 });
 
 var linksON;
 chrome.storage.sync.get("linksetting", function (result) {
-    linksON = result.linksetting;
-    console.log(result.linksetting);
-    trylinks(linksON);
+  linksON = result.linksetting;
+  trylinks(linksON);
 });
 
 
 //SCRIPT INJECTORS
-function tryhtml(htmlON){
-if (htmlON) {
+function tryhtml(htmlON) {
+  if (htmlON) {
     var h = document.createElement('script');
     h.src = chrome.extension.getURL('htmlscript.js');
     h.onload = function () {
-        this.remove();
+      this.remove();
     };
     (document.head || document.documentElement).appendChild(h);
 
-}
+  }
 }
 
-function trylinks(linksON){
-if (linksON) {
+function trylinks(linksON) {
+  if (linksON) {
     var l = document.createElement('script');
     l.src = chrome.extension.getURL('linkscript.js');
     l.onload = function () {
-        this.remove();
+      this.remove();
     };
     (document.head || document.documentElement).appendChild(l);
 
-}
+  }
 }
 
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('egg.js');
 s.onload = function () {
-    this.remove();
+  this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
--- options.js
+++ options.js
@@ -1,59 +1,61 @@
+/*eslint-env browser, jquery*/
 // Saves options to chrome.storage
 function save_options() {
-    var alltoggle = document.getElementById('alltoggle').checked;
-    var htmltoggle = document.getElementById('htmltoggle').checked;
-    var linktoggle = document.getElementById('linktoggle').checked;
-    chrome.storage.sync.set({
-        htmlsetting: htmltoggle,
-        linksetting: linktoggle,
-        allsetting: alltoggle
-    });
+  var alltoggle = document.getElementById('alltoggle').checked;
+  var htmltoggle = document.getElementById('htmltoggle').checked;
+  var linktoggle = document.getElementById('linktoggle').checked;
+  chrome.storage.sync.set({
+    htmlsetting: htmltoggle,
+    linksetting: linktoggle,
+    allsetting: alltoggle
+  });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
 
-    chrome.storage.sync.get({
-        allsetting: true,
-        htmlsetting: true,
-        linksetting: true
-    }, function (items) {
-        document.getElementById('alltoggle').checked = items.allsetting;
-        document.getElementById('htmltoggle').checked = items.htmlsetting;
-        document.getElementById('linktoggle').checked = items.linksetting;
-    });
+  chrome.storage.sync.get({
+    allsetting: false,
+    htmlsetting: false,
+    linksetting: false
+  }, function (items) {
+    document.getElementById('alltoggle').checked = items.allsetting;
+    document.getElementById('htmltoggle').checked = items.htmlsetting;
+    document.getElementById('linktoggle').checked = items.linksetting;
+  });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    //ALL-TOGGLE STUFF
-    $('.tool').change(function () {
-        //TURN OFF ALL-SWITCH IF ANY OF THE 'TOOLS' IS OFF
-        if (false == $(this).prop("checked")) {
-            $("#alltoggle").prop('checked', $(this).prop("checked"));
-        }
-        //TURN ON ALL-SWITCH IF ALL OF THE 'TOOLS' ARE ON
-        if ($('.tool:checked').length == $('.tool').length) {
-            $("#alltoggle").prop('checked', true);
-        }
-    });
+  //ALL-TOGGLE STUFF
+  $('.tool').change(function () {
+    //TURN OFF ALL-SWITCH IF ANY OF THE 'TOOLS' ARE OFF
+    if (false == $(this).prop("checked")) {
+      $("#alltoggle").prop('checked', $(this).prop("checked"));
+    }
+    //TURN ON ALL-SWITCH IF ALL OF THE 'TOOLS' ARE ON
+    if ($('.tool:checked').length == $('.tool').length) {
+      $("#alltoggle").prop('checked', true);
+    }
+  });
 
-    document.querySelector('#alltoggle').addEventListener('change', allHandler);
-    //Only enable all if it is checked (not unchecked)
-    function allHandler() {
-        if ($(this).is(':checked')) {
-            $("input:checkbox").prop('checked', $(this).prop("checked"));
-        }
+  document.querySelector('#alltoggle').addEventListener('change', allHandler);
+  //Only enable all if it is checked (not unchecked)
+  function allHandler() {
+    if ($(this).is(':checked')) {
+      $("input:checkbox").prop('checked', $(this).prop("checked"));
     }
-    //There's probably a better way to do this. Consider fixing.
-    document.querySelector('#alltoggle').addEventListener('click', disableall);
-    function disableall() {
-        if(!($(this).is(':checked'))){
-            $("input:checkbox").prop('checked', $(this).prop("checked"));
-        }
+  }
+  //There's probably a better way to do this. Consider fixing.
+  document.querySelector('#alltoggle').addEventListener('click', disableall);
+
+  function disableall() {
+    if (!($(this).is(':checked'))) {
+      $("input:checkbox").prop('checked', $(this).prop("checked"));
     }
-    // IF A TOOLBOX CHANGES, SAVE OPTIONS
-    $('input').change(save_options);
+  }
+  // IF A TOOLBOX CHANGES, SAVE OPTIONS
+  $('input').change(save_options);
 });
